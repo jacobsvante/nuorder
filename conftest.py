e9854c6f2bcb6a7705a07809e109642c1d44c7e7
Jacob Magnusson <m@jacobian.se>
2017-04-19 12:02:14 +0200

    Fix tests still failing without ~/.config/nuorder.ini

--- conftest.py
+++ conftest.py
@@ -5,9 +5,9 @@ import pytest
 def default_request_kw():
     return {
         'hostname': 'wholesale.sandbox1.nuorder.com',
-        'consumer_key': '',
-        'consumer_secret': '',
-        'oauth_token': '',
-        'oauth_token_secret': '',
-        'app_name': '',
+        'consumer_key': 'fake_consumer_key',
+        'consumer_secret': 'fake_consumer_secret',
+        'oauth_token': 'fake_oauth_token',
+        'oauth_token_secret': 'fake_oauth_token_secret',
+        'app_name': 'fake_app_name',
     }
